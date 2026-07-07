adc18a563fc4f2e5b7d24a6db6a19578df416582
pythonuser6718-netizen <pythonuser6718@gmail.com>
2026-07-06 20:14:32 -0700

    Update settings.py

--- mobileshop/settings.py
+++ mobileshop/settings.py
@@ -142,3 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://jerson-production.up.railway.app',
+]
